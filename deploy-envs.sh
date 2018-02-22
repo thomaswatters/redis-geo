5f79d287fe118354be7bab9419d66553d643a9e6
Thomas Watters <thomaswatters@gmail.com>
2018-02-21 22:48:02 -0500

    Fixing region

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -4,7 +4,7 @@
 export IMAGE_NAME=netcoreapps-redisgeo
 export IMAGE_VERSION=latest
 
-export AWS_DEFAULT_REGION=us-east-1d
+export AWS_DEFAULT_REGION=us-east-1
 export AWS_ECS_CLUSTER_NAME=default
 export AWS_VIRTUAL_HOST=ecsdemo.watters.us
 
